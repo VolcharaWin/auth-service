7d250c23a57706833db474cc60c1f11a844ab25f
laptop <boysepbin@mail.ru>
2025-01-09 18:26:18 +0300

    didn't commit main for some reason last time, commiting now

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"errors"
 	"log"
-	"net/http"
 
-	"examples.com/auth-service/hashing"
+	"examples.com/auth-service/authorization"
 	"examples.com/auth-service/server"
 	"examples.com/auth-service/storage"
-	"examples.com/auth-service/user"
 	_ "github.com/golang-jwt/jwt/v5"
 	"github.com/joho/godotenv"
 	_ "gorm.io/driver/sqlite"
@@ -26,9 +23,6 @@ func main() {
 	//sessionKey := os.Getenv("SESSION_KEY")
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-	//Custom errors
-	errExistingLogin := errors.New("this login already exists")
-
 	log.Println("====== Initialazing database and connecting to it ======")
 	db, err := storage.CreateDB()
 	if err != nil {
@@ -43,28 +37,7 @@ func main() {
 	go func() {
 		for data := range srv.LoginDataChannel {
 			go func(data server.UserData) {
-				defer close(data.Done)
-				c := data.Context
-				log.Println("You are trying to log in.")
-				exists, err := user.LoginCheck(db, data.Login)
-				if err != nil {
-					server.RespondWithError(c, 500, http.StatusText(500))
-					log.Println(err)
-					return
-				}
-				if !exists {
-					server.RespondWithError(c, 404, http.StatusText(404))
-					log.Printf("The user %s does not exist.", data.Login)
-					return
-				}
-				success, err := user.Login(db, data.Login, data.Password)
-				if err != nil || !success {
-					server.RespondWithError(c, 401, http.StatusText(401))
-					log.Println(err)
-					return
-				}
-				server.RespondWithSuccess(c, 200, "successful login")
-				log.Println("Successful authorization")
+				authorization.Login(srv, data, db)
 			}(data)
 		}
 	}()
@@ -72,32 +45,7 @@ func main() {
 	go func() {
 		for data := range srv.RegisterDataChannel {
 			go func(data server.UserData) {
-				defer close(data.Done)
-				c := data.Context
-				log.Println("You are trying to register an account.")
-				exists, err := user.LoginCheck(db, data.Login)
-				if err != nil {
-					server.RespondWithError(c, 500, http.StatusText(500))
-					log.Println(err)
-				}
-				if exists {
-					server.RespondWithError(c, 409, http.StatusText(409))
-					log.Println(errExistingLogin)
-					return
-				}
-				hashpass, err := hashing.HashPassword(data.Password)
-				if err != nil {
-					server.RespondWithError(c, 500, http.StatusText(500))
-					log.Println(err)
-				}
-				success, err := user.Registration(db, data.Login, hashpass)
-				if err != nil || !success {
-					server.RespondWithError(c, 500, http.StatusText(500))
-					log.Println(err)
-					return
-				}
-				server.RespondWithSuccess(c, 201, http.StatusText(201))
-				log.Println("Successful registration")
+				authorization.Registration(srv, data, db)
 			}(data)
 		}
 	}()
